6130f045906e0f226966794afb4b3305d68d1d45
mortada <jmortadda@gmail.com>
2023-04-21 13:42:54 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -115,6 +115,7 @@ install_XrayR() {
     go mod tidy
     export GOPROXY=https://goproxy.io,direct  &&  go build -o XrayR -ldflags "-s -w"
     cp * /usr/local/XrayR/
+    cd /usr/local/XrayR/
     wget -q -N --no-check-certificate -O /etc/XrayR/config.yml https://raw.githubusercontent.com/mortada-jafar/Xray-core/main/config.yml
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
